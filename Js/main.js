e699031b1fc1ab7a4b3b11357bb85e90bf27db8c
ranaali672 <rana.ali3699@gmail.com>
2023-12-07 19:08:09 +0200

    first commit

---
+++ Js/main.js
@@ -0,0 +1,11 @@
+var typed = new Typed('#type', {
+    // Waits 1000ms after typing "First"
+    strings: ['Hello Iam Rana <br> <span>Front End Developer</span>'],
+    typeSpeed:200,
+    loop:true,
+  });
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+// particlesJS.load('particles-js', 'Js/particlesjs-config.json', function() {
+//     console.log('callback - particles.js config loaded');
+//   });
